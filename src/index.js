df6034c53165c491d7a66f3050f76bf67b366328
vladimiroc20 <vladimiroc20@gmail.com>
2024-08-20 18:02:29 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
